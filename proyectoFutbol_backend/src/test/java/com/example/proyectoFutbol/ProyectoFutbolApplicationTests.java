bbfeca2e813325384454422ff4d08beb39d170fe
Manuel Fuentes <mfuecru1804@g.educaand.es>
2026-05-21 10:10:21 +0200

    Subida limpia y definitiva del backend

---
+++ proyectoFutbol_backend/src/test/java/com/example/proyectoFutbol/ProyectoFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
